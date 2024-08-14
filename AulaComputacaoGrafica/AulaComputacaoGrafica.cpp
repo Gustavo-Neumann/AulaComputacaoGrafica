4a96409880a379eed77c053abb25dc1d0828f23e
gustavo.pneumann <gustavo.pneumann@CAS685511F274D>
2024-08-14 20:50:49 -0300

    aula1

--- AulaComputacaoGrafica/AulaComputacaoGrafica.cpp
+++ AulaComputacaoGrafica/AulaComputacaoGrafica.cpp
@@ -1,20 +1,43 @@
-// AulaComputacaoGrafica.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
-
 #include <iostream>
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+#include <stdio.h>
+
+const GLint WIDTH = 600, HEITH = 800;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    if (!glfwInit()) {
+        printf("NAO FUNCIONOU");
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEITH, "COMPUTACAO GRAFICA", NULL, NULL);
+
+    if (!window)
+    {
+        printf("NAO ABRIU A JANELA");
+        glfwTerminate();
+        return 1;
+    }
+
+    int bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(window, &bufferWidth, &bufferHeight);
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        printf("DEU RUIM NO GLEW");
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return 1; 
+    }
+
+
+
 }
-
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
